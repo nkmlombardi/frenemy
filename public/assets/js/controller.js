b67cde1fdae6e0a7fa87d4f67d50dd3e6b55aebd
Raker <nkmlombardi@gmail.com>
2016-03-03 12:10:37 -0500

    Now displaying playerlist, gamelist and messages via Angular scope objects.

--- public/assets/js/controller.js
+++ public/assets/js/controller.js
@@ -1,25 +1,78 @@
+var socket = io.connect('http://localhost:8080');
+
 angular.module('app', [])
    .controller('MainController', function($scope) {
-        $scope.greeting = "Hello World";
-        $scope.games = [];
-        $scope.rooms = [];
 
-        socket.on('connect', function() {
-            socket.emit('adduser', 'USERNAME');
+    // Controller Object Lists
+    $scope.games = [];
+    $scope.rooms = [];
+    $scope.players = [];
+    $scope.messages = [];
+
+    $scope.current = {
+        room: {},
+        game: false,
+        player: {}
+    };
+
+
+    // Socket Listeners
+    socket.on('connect', function() {
+        $scope.messages = [];
+        socket.emit('adduser', 'USERNAME');
+    });
+
+    socket.on('updatechat', function(username, data) {
+        console.log('Event: updatechat', data);
+
+        $scope.messages.push({
+            timestamp: new Date(),
+            player: username,
+            content: data
         });
+        $scope.$apply();
+    });
 
-        socket.on('updateGamelist', function(rooms, player_room) {
-            console.log('Event: updateGamelist', rooms);
+    socket.on('updateGamelist', function(rooms, player_room) {
+        console.log('Event: updateGamelist', rooms);
 
-            $scope.rooms = rooms;
-            $scope.player_room = player_room;
+        $scope.rooms = rooms;
+        $scope.current.room = player_room;
+        $scope.$apply();
+    });
+
+    socket.on('addGamesToGamelist', function(rooms) {
+        console.log('Event: addGamesToGamelist', rooms);
+
+        $.each(rooms, function(key, room) {
+            $scope.rooms.push(room);
         });
+        $scope.$apply();
+    });
 
-        socket.on('addGamesToGamelist', function(rooms) {
-            console.log('Event: addGamesToGamelist', rooms);
+    socket.on('updatePlayerlist', function(players, self) {
+        console.log('Event: updatePlayerList', players);
 
-            $.each(rooms, function(key, room) {
-                $scope.rooms.push(room);
-            });
+        $scope.players = players;
+        $scope.current.player = self;
+        $scope.$apply();
+    });
+
+    socket.on('addPlayersToPlayerlist', function(players) {
+        console.log('Event: addPlayersToPlayerlist', players);
+
+        $.each(players, function(key, player) {
+            $scope.players.push(player);
         });
+        $scope.$apply();
+    });
+
+
+    // Browser Actions
+    $scope.sendChat = function() {
+        console.log('Event: sendChat', $scope.message);
+
+        socket.emit('sendchat', $scope.message);
+        $scope.message = '';
+    };
 });
